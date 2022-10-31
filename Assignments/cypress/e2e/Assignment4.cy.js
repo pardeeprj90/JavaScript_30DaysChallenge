4702acf4b8ef2ec276067a11332b664e7ebc955f
Pardeep <pardeep.rj90@gmail.com>
2022-11-01 01:04:40 +0530

    Assignment 4

--- Assignments/cypress/e2e/Assignment4.cy.js
+++ Assignments/cypress/e2e/Assignment4.cy.js
@@ -11,5 +11,24 @@ describe('Launch the Web Application', () => {
         cy.get("title").should('contain','Courses')
         cy.url().should('contain','login')
     })
+
+    /*
+    Task 2 – Login and Logout 
+        Open https://ineuron-courses.vercel.app/login
+        Enter username as  ineuron@ineuron.ai
+        Enter password as ineuron
+        Click on login button
+        Verify welcome message 
+        Note- Message will fade away in 10 seconds
+
+        Click on Logout
+        Verify user is logged out
+    */
+        it('Open the URL',()=>{
+            cy.visit("https://ineuron-courses.vercel.app/login")
+            cy.get("title").should('contain','Courses')
+            cy.url().should('contain','login')
+            cy.get
+        })
 });
 
